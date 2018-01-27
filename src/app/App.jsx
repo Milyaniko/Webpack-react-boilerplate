b0b8f815d70b2e454e2f5b4e287c64acdc7efcea
Nikolay Milya <Milyaniko@gmail.com>
2018-01-28 02:14:48 +0700

    The files are downloaded

---
+++ src/app/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import cat from 'images/cat';
+import dog from 'images/dog';
+import dogs from 'images/dogs';
+
+const hello = () => {
+    return (
+       <div><h1>Hi there!!!</h1></div>
+    )
+}
+
+export default hello;
+
